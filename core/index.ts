19cae2bf0bc8f383061b4c259d384671358b4663
lyxsee <zhangcjing@163.com>
2025-04-30 09:20:32 +0800

    Create index.ts

---
+++ core/index.ts
@@ -0,0 +1,31 @@
+
+export { Context } from './context';
+
+// export { Shader } from './wgsl_string/shader'
+
+export { Shader } from './wgsl/shader'
+export { ShaderTest } from './wgsl_string/shader'
+export { BindGroupLayout } from './bind_group_layout'
+export { BindGroup } from './bind_group'
+
+export { Attachment } from './attachment';
+export { Texture } from './texture';
+export { Sampler } from './sampler'
+export { Buffer } from './buffer'
+export { BufferBinding } from './buffer_binding'
+
+export { RenderTarget } from './render_target'
+export { Attribute } from './attribute'
+export { VertexState } from './vertex_state'
+export { IndexState } from './index_state'
+export { UniformGroup } from './uniform_group'
+
+export { Pipeline } from './pipeline'
+export { Pass } from './pass'
+export { PassNode } from './pass_node'
+
+export { CommandEncoder } from './command_encoder'
+
+export { ShaderStage, TextureFormat, TextureUsage, BufferUsage } from './types'
+
+export { BindGroupLayoutEntry, BufferBindingLayout, TextureBindingLayout, SamplerBindingLayout, StorageTextureBindingLayout, ExternalTextureBindingLayout } from './bind_group_layout'
